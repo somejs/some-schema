567a6b260f296062fc13f4b7f6196343c76e1852
tehfreak <tehfreak@tehfreak.com>
2012-11-28 16:54:19 +0300

    дизайн нативного способа определения класса

--- tests/design.js
+++ tests/design.js
@@ -46,3 +46,64 @@ console.log(
     baz instanceof Baz, baz instanceof Foo, bar instanceof Schema, !!baz.properties,
     baz.f === 'foo', baz.b === 'baz'
 )
+
+
+
+console.info('\n\nКлассический способ определения класса\n')
+
+var Model= function() {
+
+    this.key= Schema.Property({ verbose:'путь к данным модели', require:true })
+
+    this.loaded= false
+    this.saved= true
+
+    Schema.apply(this, arguments)
+}
+Model.prototype= Schema.prototype
+
+console.log(
+    Model instanceof Function
+)
+
+
+
+var model= null
+
+try {
+    model= new Model()
+} catch (e) {
+    console.log(
+        e instanceof Schema.Property.BadValueError
+    )
+} finally {
+    console.log(
+        model === null
+    )
+}
+
+
+
+var model= new Model({
+    key:'path/to/data',
+    loaded:true,
+
+    other:'something'
+})
+
+console.log(
+    model instanceof Model, model instanceof Schema, !!model.properties,
+    model.key == 'path/to/data', model.loaded == true, model.other == 'something'
+)
+
+try {
+    model.key= false
+} catch (e) {
+    console.log(
+        e instanceof Schema.Property.BadValueError
+    )
+} finally {
+    console.log(
+        model.key !== false, model.key == 'path/to/data'
+    )
+}
