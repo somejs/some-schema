42b90c80d591b178ab24abb2145f023c8888d584
tehfreak <tehfreak@tehfreak.com>
2012-11-28 22:37:23 +0300

    тесты

--- tests/Schema/index.js
+++ tests/Schema/index.js
@@ -1,9 +1,7 @@
 var assert= require('chai').assert
 
 module.exports= function (Schema) { return function () {
-
     describe('Библиотека экспортирует конструктор схемы, который:', function () {
-
 
         it('является функцией — Schema()', function () {
             assert.isFunction(
@@ -73,6 +71,141 @@ module.exports= function (Schema) { return function () {
         ))
 
 
-    })
 
+        describe('Классический способ определения модели', function () {
+
+            var Model= function() {
+
+                this.db= Schema.Property({ verbose:'база данных', require:true })
+                this.key= Schema.Property({ verbose:'путь к данным модели в базе данных', require:true })
+
+                this.loaded= false
+                this.saved= true
+
+                Schema.apply(this, arguments)
+            }
+            Model.prototype= Schema.prototype
+
+            describe('Конструктор схемы', function () {
+                it('бросает исключение, если значение требуемого свойства не передано', function () {
+                    var model= null
+                    try {
+                        model= new Model({
+                            db: {}, //key: 'path/to/data'
+                        })
+                    } catch (e) {
+                        assert.instanceOf(
+                            e, Schema.Property.BadValue
+                        )
+                    } finally {
+                        assert.isNull(
+                            model
+                        )
+                    }
+                })
+            })
+
+
+
+            describe('Экземпляр схемы', function () {
+
+                var db= {}
+                var model= new Model({
+                    db:db, key:'path/to/data',
+                    loaded:true,
+
+                    other:'something',
+                    empty:null,
+                })
+
+                it('имеет определенный тип', function () {
+                    assert.instanceOf(
+                        model, Model
+                    )
+                    assert.instanceOf(
+                        model, Schema
+                    )
+                })
+
+                it('имеет объявленные свойства', function () {
+                    assert.isDefined(
+                        model.db
+                    )
+                    assert.isDefined(
+                        model.key
+                    )
+                    assert.isDefined(
+                        model.loaded
+                    )
+                    assert.isDefined(
+                        model.other
+                    )
+                    assert.isDefined(
+                        model.empty
+                    )
+                })
+                it('содержит переданные значения', function () {
+                    assert.equal(
+                        'path/to/data', model.key
+                    )
+                    assert.equal(
+                        db, model.db
+                    )
+                    assert.equal(
+                        true, model.loaded
+                    )
+                    assert.equal(
+                        'something', model.other
+                    )
+                    assert.equal(
+                        null, model.empty
+                    )
+                })
+
+
+
+                describe('имеет контейнер с объявлениями свойств, который', function () {
+                    it('является объектом', function () {
+                        assert.isObject(
+                            model.properties
+                        )
+                    })
+                    it('содержит объявления свойств, чьи дескрипторы — экземпляры Schema.Property', function () {
+                        assert.isDefined(
+                            model.properties.db
+                        )
+                        assert.instanceOf(
+                            model.properties.db, Schema.Property
+                        )
+
+                        assert.isDefined(
+                            model.properties.key
+                        )
+                        assert.instanceOf(
+                            model.properties.key, Schema.Property
+                        )
+                    })
+                    it('остальных объявлений не содержит', function () {
+                        assert.isUndefined(
+                            model.properties.loaded
+                        )
+                        assert.isUndefined(
+                            model.properties.other
+                        )
+                        assert.isUndefined(
+                            model.properties.empty
+                        )
+                    })
+
+                })
+
+
+
+            })
+
+        })
+
+
+
+    })
 }}
